52c884083eb24237225612e11fec14f4ff46d796
cobywg <cobywg@gmail.com>
2023-02-19 13:59:04 -0800

    update app js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Header from './components/Header'
+import DailyTasks from './components/DailyTasks'
+import { useState } from 'react'
 function App() {
+  const [tasks, setTasks] = useState ([
+    {
+        id: 1,
+        text: 'Huberman Task 1',
+        day: 'Today',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Huberman Task 3',
+        day: 'Today 2',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Huberman Task 3',
+        day: 'Today 3',
+        reminder: true,
+    },
+    ])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header/>
+      <DailyTasks tasks = {tasks}/>
     </div>
   );
 }
+
+
+
 
 export default App;
